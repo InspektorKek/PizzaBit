a98c9b7eb1d077976ed4d431d26a99dc01f5fceb
SarahMarina22 <sndenbe22@fed.idserve.net>
2022-12-08 03:06:11 +0100

    Ignition Playground
    Lozenge Btn
    Canva to guide during coding

--- PizzaBit/View/ContentView.swift
+++ PizzaBit/View/ContentView.swift
@@ -20,11 +20,10 @@ struct ContentView: View {
     var body: some View {
         
         ZStack{
-            Image(colorScheme == .dark ? "bkg0" : "bkg0")
+            Image(colorScheme == .dark ? "bkg0" : "Canva")
                 .resizable()
                 .ignoresSafeArea()
             VStack {
-                
                 // MARK: Play Pause Soundtrack btn
                 HStack{
                     Spacer()
@@ -32,14 +31,22 @@ struct ContentView: View {
                         audioManager.playPause()
                         musicTrigger.toggle()
                         
+                        // add animation to smooth it out
+                        
                     } label: {
-                        Image(systemName: musicTrigger ? "play.circle.fill" : "pause.circle.fill")
-                            .font(.system(size: 50))
-                            .foregroundColor(Color(uiColor: .label))
-                    }.padding()
+                        LozengeBtn( cornerRadius: 0, rotationEffect: 0, frame: 50)
+                            .overlay {
+                            
+                                    Image(systemName: musicTrigger ? "play.fill" : "pause.fill")
+                                          .font(.system(size: 30))
+                                      .foregroundColor(Color(uiColor: .label))
+                                      .padding(.top,5)
+                              
+                            }
+                    }
                     
                 }
-                Spacer()
+                .offset(x: -45,y: 25)
                 
                 Text("feel")
                     .font(.caption2)
@@ -53,7 +60,6 @@ struct ContentView: View {
                     .font(.callout)
                     .bold()
                 
-                Spacer()
                 HStack {
                     ForEach(pizza){ ing in
                         Button {
@@ -62,25 +68,12 @@ struct ContentView: View {
                             playIngredientHapticsFile(ing.haptic)
                         } label: {
                             VStack {
-                                ZStack{
-                                    Circle()
-                                        .foregroundColor(.clear)
-                                        .background(RadialGradient(colors: [Color(uiColor: .systemBackground),Color(uiColor: .systemGray6),Color(uiColor: .systemGray2)], center: .center, startRadius: 0, endRadius: 80))
-                                        .overlay {
-                                            Image(ing.imgName)
-                                                .resizable()
-                                                .scaledToFit()
-                                                .frame(width: 64)
-                                                .shadow(color: Color(uiColor: .systemGray), radius: 0,x: 0,y: 3)
-                                        }
-                                        .clipShape(Circle())
-                                        .frame(width: 100)
-                                    
-                                }   .shadow(color: Color(uiColor: .systemGray6), radius: 0,x: 0,y: 6)
-                                    .shadow(color: Color(uiColor: .systemGray3), radius: 1,x: 0,y: 1)
+                                LozengeBtn(pictoName: ing.imgName, rotationEffect: 45, frame: 75)
                                 
                             }.padding()
-                                .padding(.trailing, ing.imgName == "oil0" ? 280 : 0)
+                                .padding(.trailing, ing.imgName == "oil0" ? 310 : 0)
+                            .padding(.top, ing.imgName == "tomato0" ||  ing.imgName == "mozza0" ? 45 : 0)
+                                .padding(.top, ing.imgName == "oil0" ||  ing.imgName == "basil0" ? 95 : 0)
                         }
                     }
                 }
@@ -145,6 +138,42 @@ struct ContentView: View {
     
     
 }
+
+
+struct LozengeBtn : View {
+    var pictoName : String = ""
+    var cornerRadius : Double = 20
+    var rotationEffect : Double
+    var frame : Double
+    var body: some View{
+            ZStack{
+                RoundedRectangle(cornerRadius: cornerRadius)
+                    .foregroundColor(.clear)
+                    .background(RadialGradient(colors: [Color(uiColor: .systemBackground),Color(uiColor: .systemGray6),Color(uiColor: .systemGray2)], center: .center, startRadius: 0, endRadius: 80))
+                
+                    .clipShape( RoundedRectangle(cornerRadius: 20))
+                    .frame(width: frame,height: frame)
+                    .rotationEffect(Angle(degrees: rotationEffect))
+                    .overlay {
+                        Image(pictoName)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 64)
+                            .shadow(color: Color(uiColor: .systemGray), radius: 0,x: 0,y: 3)
+                    }
+                
+            }
+            .shadow(color: Color(uiColor: .systemGray6), radius: 0,x: 0,y: 6)
+            .shadow(color: Color(uiColor: .systemGray3), radius: 1,x: 0,y: 1)
+        
+    }
+}
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
