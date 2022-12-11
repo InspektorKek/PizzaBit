35145918c9c1a460204265169e95bd5fdb7aa3d7
Mikhail Borisov <borisovdiag@gmail.com>
2022-12-11 15:20:02 +0100

    Remove useless files and fix folder name

---
+++ PizzaBit/PizzaBitApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  PizzaBit
+//
+//  Created by Mikhail Borisov on 07/12/22.
+//
+
+import SwiftUI
+
+@main
+struct PizzaBitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LevelView()
+        }
+    }
+}
